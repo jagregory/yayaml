66a76c93aeb6fe08679876a8c77f828619f53b06
James Gregory <james@jagregory.com>
2008-11-03 01:45:53 +0000

    Updated AssemblyInfo

--- YaYAML/Properties/AssemblyInfo.cs
+++ YaYAML/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("YaYAML")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Yet another YAML parser")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("YaYAML")]
-[assembly: AssemblyCopyright("Copyright ©  2008")]
+[assembly: AssemblyCopyright("Copyright © James Gregory 2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
